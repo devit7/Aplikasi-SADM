b4bb52fe81358f8fe223ce8d87c4baf23f8d9f0d
devit <60412293+devit7@users.noreply.github.com>
2025-02-26 21:18:16 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->brandName('Sistem Informasi Sekolah')
+            ->brandName('GEN-SMART')
+            
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
