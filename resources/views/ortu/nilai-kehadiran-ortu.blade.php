bea89c58dedc969ff180ff66cd7fff0f6f7f4ddd
dnabilpc <dnabilpc@gmail.com>
2025-03-17 21:55:11 +0700

    add nilai-kehadiran

---
+++ resources/views/ortu/nilai-kehadiran-ortu.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main-ortu')
+@section('content')
+    <div class="w-full">
+        <div class="flex w-full border border-blue-500 h-9">
+            <a href="#" id="nilai" class="bg-blue-500 text-white flex-1 text-center py-1 cursor-pointer">Nilai</a>
+            <a href="#" id="kehadiran" class="text-blue-500 bg-white flex-1 text-center py-1 cursor-pointer">Kehadiran</a>
+        </div>
+    </div>
+    <div>
+        <h1 class="text-3xl font-poppins">KELAS 3 SEMESTER GANJIL</h1>
+        <h2 class="text-2xl font-poppins">DINDA SULISTYA</h2>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/ortu', function () {
-    return view('ortu/index');
+    return view('ortu.index');
+});
+
+Route::get('/ortu/nilai-kehadiran', function () {
+    return view('ortu.nilai-kehadiran-ortu');
 });
