ebb4793214304c160238ef11751efe482944bbed
rickypratama17 <mrickyp28@gmail.com>
2025-03-17 22:11:23 +0700

    route list-siswa

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('walas.dashboard-walas');
 });
+Route::get('/walas/list-siswa', function () {
+    return view('walas.list-siswa');
+});
 
 Route::get('/ortu', function () {
     return view('ortu.index');
