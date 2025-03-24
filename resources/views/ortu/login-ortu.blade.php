afd1f067997ed5826dcf2b2e219c02da232169a0
Danu Wahyurinata <dhanu.wahyurinata16@gmail.com>
2025-03-24 09:29:50 +0700

    Auth Ortu

--- resources/views/ortu/login-ortu.blade.php
+++ resources/views/ortu/login-ortu.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Landing Page</title>
+    <title>Login Ortu Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.2/tailwind.min.css">
 </head>
 
@@ -13,7 +13,7 @@
     <section class="flex flex-col md:flex-row h-screen items-center">
 
         <div class="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-            <img src="/img/loginwalas.png" alt="" class="w-full h-full object-cover">
+            <img src="/img/loginortu.png" alt="" class="w-full h-full object-cover">
         </div>
 
         <div class="w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center"
@@ -24,21 +24,20 @@
                     <img src="/img/loginlogo.png" alt="Logo" class="w-32 h-32 object-cover">
                 </div>
 
-                <form class="mt-6" action="#" method="POST">
+                <form class="mt-6" action="" method="POST">
+                @csrf
                     <div>
                         <label class="">NISN</label>
-                        <input type="text" name="" id="nisn" placeholder="Masukan NISN"
+                        <input type="text" name="nisn" id="nisn" placeholder="Masukan NISN"
                             class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                             autofocus autocomplete required>
-                        <p id="nisn-error" class="hidden text-red-500 text-sm mt-1">NISN tidak boleh kosong</p>
                     </div>
 
                     <div class="mt-4">
                         <label class="">NIS</label>
-                        <input type="text" name="" id="nis" placeholder="Masukan NIS" minlength="6"
+                        <input type="text" name="nis" id="nis" placeholder="Masukan NIS" 
                             class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                             required>
-                        <p id="nis-error" class="hidden text-red-500 text-sm mt-1">NIS tidak boleh kosong</p>
                     </div>
 
                     <button type="submit"
@@ -53,28 +52,6 @@
         </div>
     </section>
     <script>
-        function validateForm() {
-            const nisn = document.getElementById('nisn').value.trim();
-            const nis = document.getElementById('nis').value.trim();
-
-            // Reset error messages
-            document.getElementById('nisn-error').classList.add('hidden');
-            document.getElementById('nis-error').classList.add('hidden');
-
-            let isValid = true;
-
-            if (nisn === "") {
-                document.getElementById('nip-error').classList.remove('hidden');
-                isValid = false;
-            }
-
-            if (nis === "") {
-                document.getElementById('password-error').classList.remove('hidden');
-                isValid = false;
-            }
-
-            return isValid;
-        }
     </script>
 </body>
 
