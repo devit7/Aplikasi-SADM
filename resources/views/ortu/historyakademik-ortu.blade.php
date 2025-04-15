22c8360e949b6b686dcb59c0855945c5ab589af3
dnabilpc <dnabilpc@gmail.com>
2025-04-15 14:07:26 +0700

    sync

--- resources/views/ortu/historyakademik-ortu.blade.php
+++ resources/views/ortu/historyakademik-ortu.blade.php
@@ -12,51 +12,9 @@
 <body>
     <x-ortu-navbar />
 
-<<<<<<< HEAD
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-    <div class="bg-white rounded-lg shadow-md p-4 flex items-center">
-      <div class="flex flex-col items-start flex-1">
-        <span class="text-gray-600 text-sm">Nilai Rata-Rata Seluruh</span>
-        <span class="text-2xl font-bold mt-1" >95.0</span>
-      </div>
-      <img src="/img/1.png" alt="Graduation Icon" class="w-12 h-12 rounded-lg">
-    </div>
-    <div class="bg-white rounded-lg shadow-md p-4 flex items-center">
-      <div class="flex flex-col items-start flex-1">
-        <span class="text-gray-600 text-sm">Peringkat Nilai Dari Tahun Lalu</span>
-        <span class="text-2xl font-bold mt-1">+10.0</span>
-      </div>
-      <img src="/img/2.png" alt="Medal Icon" class="w-12 h-12 rounded-lg">
-    </div>
-    <div class="bg-white rounded-lg shadow-md p-4 flex items-center">
-      <div class="flex flex-col items-start flex-1">
-        <span class="text-gray-600 text-sm">Persentase Kehadiran Tahun Ini</span>
-        <span class="text-2xl font-bold mt-1">90%</span>
-      </div>
-      <img src="/img/3.png" alt="Attendance Icon" class="w-12 h-12 rounded-lg">
-    </div>
-  </div>
-
-  <div class="mt-16"></div>
-  <h2 class="text-2xl font-bold mb-8 text-center">His tori Akademik</h2>
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div class="bg-white rounded-lg shadow-md overflow-hidden max-w-xs w-full flex flex-col group">
-      <div class="h-40 relative">
-        <img
-          src="logo.png"
-          alt="Kelas 1B"
-          class="w-full h-full object-cover rounded-t-lg transition-transform duration-300 group-hover:scale-105"
-        >
-      </div>
-      <!-- <div class="p-4 flex-1 flex flex-col">
-        <div class="mb-4">
-          <span class="text-gray-600 text-sm text-center block">KELAS 1B 2023</span>
-          <div class="border-b border-gray-200 mt-2"></div>
-=======
     <div class="bg-gray-100 p-8">
         <div class="flex flex-col mb-8">
             <span class="text-lg font-bold">Selamat Datang Siti!</span>
->>>>>>> c5cc76555b75000ffa741ba994588e8b664e547f
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
