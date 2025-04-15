9e3fd176368957790d51c8e5581a5e26f5098e9b
Danu Wahyurinata <dhanu.wahyurinata16@gmail.com>
2025-04-15 22:42:24 +0700

    wkwk

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Walas\ManajemenNilai;
 use App\Http\Controllers\WalasController;
 use Illuminate\Support\Facades\Route;
 
+
+
 //WALAS
 Route::get('/walas/login', function () {
     return view('walas.login-walas');
@@ -32,55 +34,17 @@ Route::prefix('walas')->middleware(['WebAkses:walikelas'])->group(function () {
 
 });
 
-<<<<<<< Updated upstream
-// ORTU
-Route::prefix('ortu')->group(function () {
-    Route::get('/profile', [ortuController::class, 'showProfile'])->name('ortu.profile');
-    route::get('/', [ortuController::class, 'index'])->name('ortu.index');
-    Route::get('/nilai-kehadiran', [ortuController::class, 'showPageNilai'])->name('ortu.nilai');
-    Route::get('/nilai-kehadiran/kehadiran', [ortuController::class, 'showPageKehadiran'])->name('ortu.kehadiran');
+Route::prefix('ortu')->middleware(['ortuAkses'])->group(function () {
+    Route::get('/profile', [OrtuController::class, 'showProfile'])->name('ortu.profile');
+    Route::get('/', [OrtuController::class, 'index'])->name('ortu.index');
+    Route::get('/nilai-kehadiran', [OrtuController::class, 'showPageNilai'])->name('ortu.nilai');
+    Route::get('/nilai-kehadiran/kehadiran', [OrtuController::class, 'showPageKehadiran'])->name('ortu.kehadiran');
     Route::get('/login', function () {
         return view('ortu.login-ortu');
     });
 });
-Route::get('/ortu/login', [ortuController::class, 'showLoginForm'])->name('ortu.login-ortu');
-Route::post('/ortu/login', [ortuController::class, 'loginOrangTua']);
-Route::post('/ortu', [ortuController::class, 'logoutOrtu'])->name('logoutortu');
 
 
-=======
-//ortu
-
-Route::get('/ortu/login', function () {
-    return view('ortu.login-ortu');
-})->name('loginOrtu');
-
-Route::post('/ortu/login', [AuthController::class, 'loginOrtu']);
-Route::post('/logoutortu', [AuthController::class, 'logoutOrtu'])->name('ortu.logout');
-
-
-Route::get('/ortu', function () {
-    return view('ortu.index');
-});
-Route::get('/Profile', function () {
-    return view('ortu.ProfileSiswa-Ortu');
-});
-
-Route::get('/ortu/history-akademik', function () {
-    return view('ortu.historyakademik-ortu');
-});
-
-Route::get('/ortu/nilai-kehadiran', [ortuController::class, 'getNilai']);
-Route::get('/ortu/nilai-kehadiran/kehadiran', [ortuController::class, 'getAbsen']);
->>>>>>> Stashed changes
-
-route::get('/', function () {
-    return view('ortu.historyakademik-ortu');
-});
-
-
-// Route::get('/walas/list-siswa', function () {
-//     return view('walas.list-siswa');
-// });
-
-//panel orang tua
+Route::get('/ortu/login', [OrtuController::class, 'showLoginForm'])->name('ortu.login-ortu');
+Route::post('/ortu/login', [OrtuController::class, 'loginOrangTua']);
+Route::post('/ortu', [OrtuController::class, 'logoutOrtu'])->name('logoutortu');
